eafeb6482a401a95c0dd1be32c6521750202db0e
wanqhuang <493106957@qq.com>
2016-06-02 22:36:56 +0800

    a sql text for test

---
+++ insert_account.sql
@@ -0,0 +1,11 @@
+declare
+   v_count number;
+begin
+   select count(*) into v_count from dept where deptid = '12';
+   if v_count>1 then
+       dbms_output.putline('I love git');
+   end if;
+exception
+when no_data_found then
+   dbms_output.putline('please check the table dept,tks');
+end;
