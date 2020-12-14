bfb2b92aae0a5a9457e0ecaa3fcc15ff2a27657d
wanqDerek <1281258473@qq.com>
2020-12-14 20:13:26 +0800

    测试TorGit

--- insert_account.sql
+++ insert_account.sql
@@ -5,7 +5,7 @@ begin
    if v_count>1 then
        dbms_output.putline('I love git');
        dbms_output.putline('add a new line');
-   end if;
+   end if;  --IF结构结束
 exception
  when no_data_found then
    dbms_output.putline('please check the table dept,tks');
